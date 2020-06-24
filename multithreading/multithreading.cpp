e0dff86228674a5fcb30c4e7018552ac128642d7
sozzo <o.hryhorovych@gmail.com>
2020-06-24 15:19:17 +0300

    removed old code and went to do UML diagram

--- multithreading/multithreading.cpp
+++ multithreading/multithreading.cpp
@@ -3,21 +3,7 @@
 #include "A.h"
 #include "B.h"	
 using namespace std;
-int step_i = 0;
-/*B merge() {//reused code
-	for (int h = 2; h <= max_threads; h++)
-	{
-		for (int i = 0; i < h; i++)
-		{
-			pthread_create(thr_id[i], NULL, multiplyMatrices, i);
-		}
 
-		for (int i = 0; i < h; i++)
-		{
-			pthread_join(thr_id[i], NULL);
-		}
-	}
-}*/
 int main()
 {
 	int lengthOfArray = 0;
