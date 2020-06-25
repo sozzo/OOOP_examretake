e124c573bec0b3b0cbd78f4477ba6849c15fecd9
sozzo <o.hryhorovych@gmail.com>
2020-06-25 19:01:47 +0300

    finished, todo debug

--- multithreading/multithreading.cpp
+++ multithreading/multithreading.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include<thread>
+#include <future>
 #include "A.h"
 #include "B.h"	
 using namespace std;
-B genTransferMerge(int x) {
+void genTransferMerge(int x, B &toMerge) {//toMerge is the variable that we merge into
 	vector<A> generator;
 	vector<B> transformer;
 	B res;
@@ -19,16 +20,18 @@ B genTransferMerge(int x) {
 		transformer.push_back(b);
 	}
 	res = res.merge(transformer, x);
-	return res;
-
+	toMerge.a.x = res.a.x;
 }
 int main()
 {
-	int lengthOfArray = 0;
+	int amountOfVariables = 0;// must be redefined to 1 or greater by the user
 	unsigned int numberOfThreads = thread::hardware_concurrency();//determines effective number of concurrent threads posible
 	//cout << numberOfThreads << endl;
 	char threadAnswer;
-	cout << 17 / 4;
+	vector<thread> threads;//vector of threads
+	vector<B>inBetweenRes;//results after closing each vector
+	B res(0);
+	res.a.x = 1;
 	cout << "Do you want to change the amount of threads(set to the number of cores by default)? y/n" << endl;
 	cin >> threadAnswer;
 	if (threadAnswer == 'y') {
@@ -43,15 +46,39 @@ int main()
 			cout << "wrong input, amount of threads wasn't changed";
 		}
 	}
-	vector<thread> threads;
-	while (lengthOfArray < 1) {
+	for (int i = 0; i < numberOfThreads; i++) {
+		inBetweenRes.push_back(B(1));
+	}
+
+	while (amountOfVariables < 1) {
 		cout << "how many As shall we generate(can't be less than 1)";
-		cin >> lengthOfArray;
-		if (lengthOfArray < 1) {
+		cin >> amountOfVariables;
+		if (amountOfVariables < 1) {
 			cout << "wrong input" << endl;
 		}
 	}
-	cout<<genTransferMerge(lengthOfArray).a.x;
+
+
+	if (amountOfVariables % numberOfThreads == 0) {
+		for (int i = 0; i < numberOfThreads; i++) {
+			threads.push_back(thread(genTransferMerge, amountOfVariables / numberOfThreads,inBetweenRes.at(i)));//pushing same amount of elements to all threads
+		
+		}
+	}else{
+		for (int i = 0; i < numberOfThreads - 1; i++) {
+			threads.push_back(thread(genTransferMerge, (amountOfVariables / numberOfThreads)+1, inBetweenRes.at(i)));//pushing different amount of elements to last thread
+		}
+		threads.push_back(thread(genTransferMerge, (amountOfVariables - (amountOfVariables / numberOfThreads) + 1) * (numberOfThreads-1), inBetweenRes.at(numberOfThreads-1)));//calculating number of leftover A variables
+	}
+	for (unsigned int i = 0; i < numberOfThreads; ++i)
+	{
+		if (threads.at(i).joinable())
+			threads.at(i).join();
+	}
+	for (int i = 0; i < numberOfThreads; i++) {
+		res.a.x = (res.a.x * inBetweenRes.at(i).a.x);
+	}
+	cout<<"result is " << res.a.x<<endl;
 	
     std::cout << "Hello World!\n";
 	
